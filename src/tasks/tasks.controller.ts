8771064f31a5b6f693f9031bd86a11673d41b9b4
eren kiraz <erengkiraz@gmail.com>
2021-12-22 15:03:25 +0300

    pipes, validations added

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,7 +10,8 @@ import {
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-fiter-dto';
-import { Task, TaskStatus } from './task.model';
+import { UpdateTaskStatusDTO } from './dto/update-task-status.dto';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -44,8 +45,9 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() updateTaskStatusDTO: UpdateTaskStatusDTO,
   ): Task {
+    const { status } = updateTaskStatusDTO;
     return this.tasksService.updateTaskStatus(id, status);
   }
 }
